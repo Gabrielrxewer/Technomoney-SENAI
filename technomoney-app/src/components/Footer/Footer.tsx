94d2e8f731013f18e65c181b7fb6c2160ed1e567
Gabriel Roewer <Gabriel Roewer@Lap01-Mondai.ska.com.br>
2025-04-30 21:12:31 -0300

    Criação do layout do Footer: adicionar footer fixo com copyright - Commited by: DeboraRomitti85

---
+++ technomoney-app/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <h1>&copy; 2025 Technmoney</h1>
+    </footer>
+  );
+};
+
+export default Footer;
