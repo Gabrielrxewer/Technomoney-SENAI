a85d6a9630c75d6b1bfb46adf2756df9ff61e8db
DeboraRomitti85 <deboraromitti@gmail.com>
2025-05-19 21:17:30 -0300

    Atualização do layout do menu: criação e inclusão de novas opções – ajustes em CSS

--- technomoney-app/src/components/Menu/Menu.tsx
+++ technomoney-app/src/components/Menu/Menu.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Menu.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faDesktop,
+  faInfoCircle,
+  faTools,
+  faTag,
+  faPhone,
+  faNewspaper,
+  faQuestionCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Menu.css";
 
 interface MenuProps {
   onClose: () => void;
@@ -10,16 +21,60 @@ const Menu: React.FC<MenuProps> = ({ onClose }) => {
   return (
     <div className="menu">
       <div className="menu-content">
-        {/* Botão de fechar */}
-        <button onClick={onClose} className="close-btn">X</button>
-
-        {/* Lista de Links */}
+        <button onClick={onClose} className="close-btn">
+          X
+        </button>
         <ul>
           <li>
-            <Link to="/" onClick={onClose}>Acessar Home</Link>
+            <Link to="/" onClick={onClose}>
+              <FontAwesomeIcon icon={faHome} style={{ marginRight: 8 }} />
+              Acessar Home
+            </Link>
           </li>
           <li>
-            <Link to="/register" onClick={onClose}>Acessar a Plataforma</Link>
+            <Link to="/regiter" onClick={onClose}>
+              <FontAwesomeIcon icon={faDesktop} style={{ marginRight: 8 }} />
+              Acessar a Plataforma
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" onClick={onClose}>
+              <FontAwesomeIcon icon={faInfoCircle} style={{ marginRight: 8 }} />
+              Sobre / Quem Somos
+            </Link>
+          </li>
+          <li>
+            <Link to="/" onClick={onClose}>
+              <FontAwesomeIcon icon={faTools} style={{ marginRight: 8 }} />
+              Serviços / Produtos
+            </Link>
+          </li>
+          <li>
+            <Link to="/" onClick={onClose}>
+              <FontAwesomeIcon icon={faTag} style={{ marginRight: 8 }} />
+              Preços / Planos
+            </Link>
+          </li>
+          <li>
+            <Link to="/" onClick={onClose}>
+              <FontAwesomeIcon icon={faPhone} style={{ marginRight: 8 }} />
+              Contato / Fale Conosco
+            </Link>
+          </li>
+          <li>
+            <Link to="/" onClick={onClose}>
+              <FontAwesomeIcon icon={faNewspaper} style={{ marginRight: 8 }} />
+              Blog / Notícias
+            </Link>
+          </li>
+          <li>
+            <Link to="/" onClick={onClose}>
+              <FontAwesomeIcon
+                icon={faQuestionCircle}
+                style={{ marginRight: 8 }}
+              />
+              Ajuda / FAQ
+            </Link>
           </li>
         </ul>
       </div>
