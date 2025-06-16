e919ed0db929e2edd132b8cff1fcdf7f0e97f9ad
Gabriel Roewer <Gabriel Roewer@Lap01-Mondai.ska.com.br>
2025-06-16 20:26:50 -0300

    FEAT: Filtrando por tag e retornando os dados sempre atualizados

--- technomoney-api/src/routes/assetRoutes.ts
+++ technomoney-api/src/routes/assetRoutes.ts
@@ -1,19 +1,19 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/authMiddleware";
 import {
-  createAsset,             
+  createAsset,
   getAllAssets,
-  getAssetsById,
   getAssetsByDate,
   getAssetsSortedByVolume,
   getAssetsSortedByPrice,
+  getAssetByTag,
 } from "../controllers/assetController";
 
 const router = Router();
 
 router.post("/assets", createAsset);
 router.get("/assets", getAllAssets);
-router.get("/assets/:id", getAssetsById);
+router.get("/assets/:tag", getAssetByTag);
 router.get("/assets/date/:date", authenticate, getAssetsByDate);
 router.get("/assets/sorted/volume", authenticate, getAssetsSortedByVolume);
 router.get("/assets/sorted/price", authenticate, getAssetsSortedByPrice);
