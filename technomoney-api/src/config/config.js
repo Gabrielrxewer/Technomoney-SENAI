14178287042f7732aad247279639f29137c951e8
Gabriel Roewer <Gabriel Roewer@Lap01-Mondai.ska.com.br>
2025-07-15 19:30:52 -0300

    HOTFIX: Correção DB_DRIVER config corrompido

--- technomoney-api/src/config/config.js
+++ technomoney-api/src/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DATABASE || '',
     server: process.env.DB_HOST || '',
-    dialect: process.env.DB_DIALECT || '',
+    dialect: process.env.DB_DRIVER || '',
     dialectOptions: {
       options: {
         encrypt: false,
